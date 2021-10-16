6431cd5f2c6c8a1a1d458a4c2e7075c234fd8732
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-10-16 19:35:31 +0200

    Add files via upload

--- customers.R
+++ customers.R
@@ -6,44 +6,51 @@ for(i in pks){
   }
 }
 
-df <- read_stata("Z://DesktopC//LUMSA//2//Data Mining//customers.dta")
+custom <- read_stata("Z://DesktopC//LUMSA//2//Data Mining//customers.dta")
 
-df$gender <- as.factor(df$gender)
-df$married <- as.factor(df$married)
-df$catalogpromo <- round(df$catalogpromo)
+custom %<>% filter(monthnumb < 37)
 
-df %<>% filter(monthnumb < 25)
+custom$gender <- as.factor(custom$gender)
+custom$married <- as.factor(custom$married)
+custom$catalogpromo <- round(custom$catalogpromo)
+custom$stdMonth <- round(scale(custom$monthnumb),2)
+custom$winter <- ifelse(custom$stdMonth==-0.72,1,
+                        ifelse(custom$stdMonth==-0.63,1,
+                               ifelse(custom$stdMonth==0.43,1,
+                                      ifelse(custom$stdMonth==0.53,1,
+                                             ifelse(custom$stdMonth==1.59,1,
+                                                    ifelse(custom$stdMonth==1.68,1,0))))))
 
-df %>% group_by(hh_key) %>% summarize(n_ppl = n())
+custom %>% group_by(hh_key) %>% summarize(n_ppl = n())
 
-keyGroup<-df %>% group_by(hh_key) %>% summarize(items = sum(item),
+keyGroup <-custom %>% group_by(hh_key) %>% summarize(items = sum(item),
                                                 catalogs = sum(catalogpromo),
                                                 retails = sum(retailpromo),
-                                                prices = sum(pricepromo))
+                                                prices = sum(pricepromo),
+                                                itemPromo = sum(item[which(pricepromo>=1&item>0)]))
 
-df %<>% group_by(monthnumb) %>% 
-  summarize(avgItem = mean(item),
-            avgCatal = mean(catalogpromo),
-            avgRetail = mean(retailpromo)) %>% 
+monthGroup <-custom %>% group_by(monthnumb) %>% summarize(avgItem = mean(item),
+                                avgCatal = mean(catalogpromo),
+                                avgRetail = mean(retailpromo)) %>% 
   mutate(year = ifelse(monthnumb < 13, 1, ifelse(monthnumb < 25, 2, ifelse(monthnumb < 37, 3, "   "))),
-         fixedMon = c(seq(1,12,by=1),seq(1,12,by=1),seq(1,12,by=1)),
-         winter = ifelse(fixedMon%%12==0, 1, ifelse(fixedMon%%11==0, 1, 0)))
+  fixedMon = c(seq(1,12,by=1),seq(1,12,by=1),seq(1,12,by=1)),
+  winter = ifelse(fixedMon%%12==0, 1, ifelse(fixedMon%%11==0, 1, 0)))
 
-p1 <- ggplot(data = df, aes(x = monthnumb, y = avgItem, group=1)) + 
+p1 <- ggplot(data = monthGroup, aes(x = monthnumb, y = avgItem, group=1)) + 
       geom_line() +
       scale_x_continuous(name = "time [years]",breaks = c(0,12,24,36), labels = c(0,1,2,3)) +
       theme(plot.title = element_text(hjust = 0.5)) +
       labs(title = "Purchases",
        y = "av. number")
 
-p2 <- ggplot(data = df, aes(x = monthnumb, y = avgCatal, group=1)) + 
+p2 <- ggplot(data = monthGroup, aes(x = monthnumb, y = avgCatal, group=1)) + 
       geom_line() +
       scale_x_continuous(name = "time [years]",breaks = c(0,12,24,36), labels = c(0,1,2,3)) +
       theme(plot.title = element_text(hjust = 0.5)) +
       labs(title = "Catalog promotions",
        y = "av. number")
 
-p3 <- ggplot(data = df, aes(x = monthnumb, y = avgRetail, group=1)) + 
+p3 <- ggplot(data = monthGroup, aes(x = monthnumb, y = avgRetail, group=1)) + 
       geom_line() +
       scale_x_continuous(name = "time [years]",breaks = c(0,12,24,36), labels = c(0,1,2,3)) +
       theme(plot.title = element_text(hjust = 0.5)) +
@@ -53,39 +60,43 @@ p3 <- ggplot(data = df, aes(x = monthnumb, y = avgRetail, group=1)) +
 grid.arrange(p1, p2, p3, ncol=1)
 
 
-ggplot(data = df, aes(x = monthnumb, y = avgItem, group=as.factor(winter), color=as.factor(winter))) + 
+ggplot(data = monthGroup, aes(x = monthnumb, y = avgItem, group=as.factor(winter), color=as.factor(winter))) + 
   geom_boxplot() +
   scale_x_continuous(name = "Month",breaks = c(13,26), labels = c("Non Winter","Winter")) +
   theme() +
   labs(color = "Winter")
 
-model5 <- hurdle(item ~ as.factor(monthnumb)+as.factor(retailpromo)+catalogpromo+as.factor(gender)+as.factor(married)+as.factor(income), data = customers, dist = "negbin")
-AIC(model,model2,model3,model4,model5)
-BIC(model,model2,model3,model4,model5)
 
-evaluateModel <- function(model1, model2, data){
-  TZero <- sum(data$item == 0)
-  Pzero1 <- sum(predict(model1, type = "prob")[,1])
-  Pzero2 <- sum(predict(model2, type = "prob")[,1])
-  Pones1 <- sum(predict(model1, type = "prob")[,2])
-  Pones2 <- sum(predict(model2, type = "prob")[,2])
-  Pduos1 <- sum(predict(model1, type = "prob")[,3])
-  Pduos2 <- sum(predict(model2, type = "prob")[,3])
-  res = paste("True zeros:",as.numeric(TZero),
-        "Predicted zeros from model-1:",as.numeric(Pzero1),
-          "Predicted zeros from model-2 ",as.numeric(Pzero2),
-           "Predicted ones from model-1:",as.numeric(Pones1),
-           "Predicted ones from model-2:",as.numeric(Pones2),
-           "Predicted duos from model-1:",as.numeric(Pduos1),
-           "Predicted duos from model-2:",as.numeric(Pduos2),sep = "\n")
-  return(cat(res))
+
+ggplot(data = custom, aes(x = hh_key, y = item, group=as.factor(pricepromo), color=as.factor(pricepromo))) + 
+  geom_boxplot()+
+  scale_x_continuous(name = "time [years]",breaks = c(0,12,24,36), labels = c(0,1,2,3))+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title="Purchased items per price promotion over time",y="n. items",color = "N° of price promotions")
+
+as.matrix(tapply(custom$item, list(gender,married), mean))
+#############################################################
+formula=item~retailpromo+catalogpromo+gender+married+winter
+fm_pois <- glm(formula, data = custom, family = poisson)
+fm_qpois <- glm(formula, data = custom, family = quasipoisson)
+fm_nbin <- glm.nb(formula, data = custom)
+fm_hurdle <- hurdle(formula, data = custom, dist = "negbin")
+fm_zinb <- zeroinfl(formula, data = custom, dist = "negbin")
+#############################################################
+summaryModels <- function(numb){
+  fm <- list("ML-Pois" = fm_pois, "Quasi-Pois" = fm_qpois, "NB" = fm_nbin,
+             "Hurdle-NB" = fm_hurdle, "ZINB" = fm_zinb)
+  invisible(readline(prompt="Press [enter] for summary of models"))
+  print(sapply(fm, function(x) coef(x)[1:12]))
+  invisible(readline(prompt="Press [enter] for estimated standard errors of models"))
+  print(cbind("ML-Pois" = sqrt(diag(vcov(fm_pois))),"Adj-Pois" = sqrt(diag(sandwich(fm_pois))),sapply(fm[-1], function(x) sqrt(diag(vcov(x)))[1:12])))
+  invisible(readline(prompt="Press [enter] for logLik and Df analysis"))
+  print(rbind(logLik = sapply(fm, function(x) round(logLik(x), digits = 0)),Df = sapply(fm, function(x) attr(logLik(x), "df"))))
+  invisible(readline(prompt="Press [enter] for real vs fitted of numb"))
+  print(round(c("Obs" = sum(item < numb+1),"ML-Pois" = sum(dpois(numb, fitted(fm_pois))),"NB" = sum(dnbinom(numb, mu = fitted(fm_nbin), size = fm_nbin$theta)),"NB-Hurdle" = sum(predict(fm_hurdle, type = "prob")[,numb+1]),"ZINB" = sum(predict(fm_zinb, type = "prob")[,numb+1]))))
+  invisible(readline(prompt="Press [enter] for zero-augmented models (zero-part model)"))
+  print(t(sapply(fm[4:5], function(x) round(x$coefficients$zero, digits = 3))))
 }
-
-expCoef <- exp(coef((model6)))
-expCoef <- matrix(expCoef, ncol = 2)
-rownames(expCoef) <- names(coef(model7))
-colnames(expCoef) <- c("Count_model","Zero_hurdle_model")
-expCoef
 
 
 rootogram(model8, max = 30)
