84ec6a3089f1f31186f9a5eba53379d33d02acaf
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-10-20 22:58:31 +0200

    Add files via upload

--- Clustering/clustering.R
+++ Clustering/clustering.R
@@ -1,21 +1,30 @@
 load("Z:/DesktopC/LUMSA/2/Data Mining/Clustering/ClusterData_L31.RData")
 
 preProcessing <- function(df){
-  df <- df[, c(-1,-2)]
-  df = data.frame(scale(df))
+  df[3:10] <- scale(df[3:10])
+  df <- df[-c(1,2)]
   return(df)
 }
 
 catReg <- function(df){
-  #areaNames <- c("NApu","Cala","SApu","Sici","ISar","CSar","ELig","WLig","Umbr")
   regionNames <- c("Sud","Isola","Nord")
   for (i in unique(df$Region)){
     df$Region[df$Region == i] <- regionNames[i]
   }
   return(df)
 }
-stdOlive <- preProcessing(olive)
-olive <- catReg(olive)
+
+catArea <- function(df){
+  areaNames <- c("NApu","Cala","SApu","Sici","ISar","CSar","ELig","WLig","Umbr")
+  for (i in unique(df$Area)){
+    df$Area[df$Area == i] <- areaNames[i]
+  }
+  return(df)
+}
+
+df <- catReg(olive)
+df <- catArea(df)
+df <- preProcessing(df)
 
 x.dist1=dist(stdOlive,method="euclidean")
 x.dist2=dist(stdOlive,method="manhattan")
@@ -48,7 +57,7 @@ t(table(olive[,1],k.olive$cluster))
 
 table(olive[,1]) #true
 
-################################################################################
+###############################INTERNET#########################################
 library(tidyverse)  # data manipulation
 library(cluster)    # clustering algorithms
 library(factoextra) # clustering algorithms & visualization
@@ -102,3 +111,205 @@ grid.arrange(p1, p2, p3, nrow = 2)
 wss
 sil
 final
+####################################BOOK########################################
+install.packages(c("cluster","factorextra"))
+dist.eucl <- dist(df, method = "euclidean")
+round(as.matrix(dist.eucl)[1:9, 1:9], 1)
+dist.cor <- get_dist(df, method = "pearson")
+round(as.matrix(dist.cor)[1:9, 1:9], 1)
+fviz_dist(dist.eucl)
+fviz_nbclust(df, kmeans, method = "wss") +
+  geom_vline(xintercept = 4, linetype = 2)
+set.seed(123)
+km.res <- kmeans(df, 4, nstart = 25)
+print(km.res)
+aggregate(df, by=list(cluster=km.res$cluster),mean)
+dd <- cbind(olive, cluster=km.res$cluster)
+head(dd)
+km.res$size
+km.res$centers
+fviz_cluster(km.res, data=df,
+             palette=c("#2E9FDF","#00AFBB","#E7B800","#FC4E07"),
+             ellipse.type = "euclid",
+             star.plot=TRUE,
+             repel=TRUE,
+             ggtheme = theme_minimal())
+
+fviz_nbclust(df, pam, method = "silhouette")+
+  theme_classic()
+pam.res <- pam(df, 5)
+print(pam.res)
+dd <- cbind(df, cluster=pam.res$cluster)
+pam.res$medoids
+head(pam.res$cluster)
+fviz_cluster(pam.res,
+             palette=c("#2E9FDF","#00AFBB","#E7B800","#FC4E07","purple"),
+             ellipse.type = "t",
+             repel=T,
+             ggtheme = theme_classic())
+
+fviz_nbclust(df, clara, method = "silhouette")+
+  theme_classic()
+clara.res <- clara(df, 5, samples=50, pamLike=TRUE)
+print(clara.res)
+dd <- cbind(df, cluster=clara.res$cluster)
+head(clara.res$clustering,10)
+clara.res$medoids
+fviz_cluster(clara.res,
+             palette=c("#2E9FDF","#00AFBB","#E7B800","#FC4E07","purple"),
+             ellipse.type = "t",
+             geom="point", pointsize = 1,
+             ggtheme = theme_classic())
+res.hc <- hclust(d = dist.eucl, method = "ward.D2")
+fviz_dend(res.hc, cex=.5)
+res.hc2 <- hclust(dist.eucl, method = "average")
+grp <- cutree(res.hc, k=5)
+head(grp, n=5)
+table(grp)
+fviz_dend(res.hc, k=5,
+          cex = 0.5,
+          k_colors = c("#2E9FDF","#00AFBB","#E7B800","#FC4E07","purple"),
+          color_labels_by_k = TRUE,
+          rect = TRUE)
+fviz_cluster(list(data=df, cluster=grp),
+             palette=c("#2E9FDF","#00AFBB","#E7B800","#FC4E07","purple"),
+             ellipse.type = "convex",
+             repel=T,
+             show.clust.cent = F,
+             ggtheme = theme_minimal())
+res.agnes <- agnes(x=df,
+                   stand = T,
+                   metric = "euclidean",
+                   method = "ward")
+res.diana <- diana(x=df, 
+                   stand = T,
+                   metric = "euclidean")
+fviz_dend(res.agnes, cex=0.6, k=5)
+install.packages("dendextend")
+library(dendextend)
+res.dist <- dist(df, method = "euclidean")
+hc1 <- hclust(res.dist,method = "average")
+hc2 <- hclust(res.dist,method = "ward.D2")
+dend1 <- as.dendrogram(hc1)
+dend2 <- as.dendrogram(hc2)
+dend_list <- dendlist(dend1, dend2)
+tanglegram(dend1, dend2)
+cor.dendlist(dend_list, method = "cophenetic")
+cor.dendlist(dend_list, method = "baker")
+cor_cophenetic(dend1, dend2)
+cor_bakers_gamma(dend1,dend2)
+dend1 <- df %>% dist %>% hclust("complete") %>% as.dendrogram()
+dend2 <- df %>% dist %>% hclust("single") %>% as.dendrogram()
+dend3 <- df %>% dist %>% hclust("average") %>% as.dendrogram()
+dend4 <- df %>% dist %>% hclust("centroid") %>% as.dendrogram()
+dend_list <- dendlist("Complete"=dend1,"Single"=dend2,"Average"=dend3,"Centroid"=dend4)
+cors <- cor.dendlist(dend_list)
+round(cors, 2)
+corrplot::corrplot(cors, "pie", "lower")
+dd <- dist(df, method = "euclidean")
+hc <- hclust(dd, method = "ward.D2")
+require("igraph")
+fviz_dend(hc, k=4, k_colors = "jco",
+          type="phylogenic",repel = T)
+fviz_dend(hc, k=4, k_colors = "jco",
+          type="phylogenic",repel = T,
+          phylo_layout = "layout.gem")
+install.packages("clustertend")
+fviz_pca_ind(prcomp(df),habillage = olive$Area, palette = "jco",
+             geom = "point",ggtheme=theme_classic(),legend="bottom")
+fviz_pca_ind(prcomp(df),geom = "point",ggtheme=theme_classic())
+km.res1 <- kmeans(df, 5)
+fviz_cluster(list(data=df, cluster=km.res1$cluster),
+             ellipse.type = "norm",
+             geom = "point",
+             stand=F,
+             palette="jco",
+             ggtheme = theme_minimal())
+fviz_dend(hclust(dist(df)),k=5, k_colors = "jco",
+          as.ggplot=T, show_labels = F)
+set.seed(123)
+hopkins(df, n=nrow(df)-1)
+fviz_dist(dist(df),show_labels = F)
+#install.packages("NbClust")
+library(NbClust)
+fviz_nbclust(df, kmeans, method = "wss")+
+  geom_vline(xintercept = 4, linetype=2)
+fviz_nbclust(df, kmeans, method = "silhouette")
+set.seed(123)
+fviz_nbclust(df, kmeans, nstart=25, method = "gap_stat", nboot = 500)
+nb <- NbClust(df, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans")
+fviz_nbclust(nb)
+km.res <- eclust(df, "kmeans",k=5, nstart=25, graph = F)
+fviz_cluster(km.res, geom="point",ellipse.type = "norm",palette="jco",ggtheme=theme_minimal())
+hc.res <- eclust(df, "hclust",k=5, hc_metric = "euclidean",hc_method = "ward.D2",graph = F)
+fviz_dend(hc.res,show_labels = F, palette = "jco",as.ggplot=T)
+fviz_silhouette(km.res, palette="jco",ggtheme=theme_minimal())
+silinfo <- km.res$silinfo
+names(silinfo)
+head(silinfo$widths[, 1:3],10)
+silinfo$clus.avg.widths
+silinfo$avg.width
+km.res$size
+library(fpc)
+km_stats <- cluster.stats(dist(df), km.res$cluster)
+km_stats$dunn
+km_stats
+table(olive$Area, km.res$cluster)
+areas <- as.numeric(olive$Area)
+clust_stats <- cluster.stats(d = dist(df),
+                             areas,
+                             km.res$cluster)
+clust_stats$corrected.rand
+clust_stats$vi
+pam.res <- eclust(df, "pam",k=5, graph = F)
+table(olive$Area, pam.res$cluster)
+cluster.stats(d = dist(df),
+                  areas,
+                  instkm.res$cluster)$vi
+res.hc <- eclust(df, "hclust",k=5,graph = F)
+table(olive$Area, pam.res$cluster)
+cluster.stats(d = dist(df),
+                  areas,
+                  res.hc$cluster)$vi
+install.packages("clValid")
+library(clValid)
+clmethods <- c("hierarchical","kmeans","pam")
+intern <- clValid(df, nClust = 2:6,
+                  clMethods = clmethods, validation = "internal")
+summary(intern)
+stab <- clValid(df, nClust = 2:6,
+                  clMethods = clmethods, validation = "stability")
+summary(intern)
+optimalScores(stab)
+install.packages("pvclust")
+library(pvclust)
+set.seed(123)
+res.pv <- pvclust(df, method.dist = "cor",
+                  method.hclust = "average",nboot = 10)
+plot(res.pv, hang=-1, cex=0.5)
+pvrect(res.pv)
+clusters <- pvpick(res.pv)
+clusters
+install.packages("parallel")
+library(parallel)
+c1 <- makeCluster(2, type="PSOCK")
+res.pv <- parPvclust(c1, df, nboot = 1000)
+stopCluster(c1)
+res.hk<-hkmeans(df, 5)
+fviz_cluster(res.hk, palette="jco",repel = T, ggtheme=theme_classic())
+fviz_dend(res.hk, cex=0.6, palette="jco",
+          rect=T, rect_border = "jco",rect_fill = T)
+res.fanny <- fanny(df, 5)
+head(res.fanny$membership, 5)
+fviz_silhouette(res.fanny, palette="jco",ggtheme=theme_minimal())
+library(mclust)
+mc <- Mclust(df)
+summary(mc)
+mc$modelName
+mc$G
+head(mc$z, 30)
+head(mc$classification, 30)
+fviz_mclust(mc, "BIC",palette="jco")
+fviz_mclust(mc, "classification",geom="point",palette="jco",poitsize=1.5)
+fviz_mclust(mc, "uncertainty",palette="jco")
+table(olive[,2],mc$classification)
