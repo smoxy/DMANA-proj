3beff4cccbfe7b409fd79460949a1733ec46000e
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-10-25 13:01:58 +0200

    Update metrics.R

--- metrics.R
+++ metrics.R
@@ -1,4 +1,4 @@
-confusion <- matrix(sample.int(10, size = 4*4, replace = TRUE), nrow = 4, ncol = 4)
+confusion <- matrix(sample.int(10, size = 2*2, replace = TRUE), nrow = 2, ncol = 2)
 
 metrics <- function(matrix){
   diag <- c()
@@ -8,12 +8,29 @@ metrics <- function(matrix){
     diag <- append(diag,matrix[i,i])
     rowsum <- append(rowsum,sum(matrix[i,]))
     colsum <- append(colsum,sum(matrix[,i]))
-  #cat(paste( ,sep="\n"))
-  #return(df)
-  #cat(paste("Accuracy:",diag/rowsum))
   }
-  #print(sum(diag))
-  print("Acc")
-  print(sum(diag)/sum(rowsum))
+  TP <- diag(matrix)
+  FP <- colSums(matrix) - diag(matrix)
+  FN <- rowSums(matrix) - diag(matrix)
+  TN <- sum(matrix) - (FP + FN + TP)
+  accuracy <- sum(TP) / sum(TP + FN)
+  sensitivity <- TP / (TP + FN)
+  specificity <- TN / (TN+FP) 
+  precision <- TP / (TP + FP)
+  recall <- TP / (TP + FN)
+  beta <- as.numeric(readline(prompt="Beta: "))
+  f1 <- ifelse(precision + recall == 0, 0, 2 / (beta * (1/precision) + (beta * 1/recall)))
+  f1[is.na(f1)] <- 0
+  
+  cat("Acc:", accuracy)
+  cat("\n")
+  cat("Sensitivity:", sensitivity)
+  cat("\n")
+  cat("Specificity:", specificity)
+  cat("\n")
+  cat("Precision:", precision)
+  cat("\n")
+  cat("Recall:", recall)
+  cat("\n")
+  cat("F1:", f1)
 }
-
