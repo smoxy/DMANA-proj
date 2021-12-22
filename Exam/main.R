9b7633836171aabd27a9fce8900e19ba0c6b0f79
Matteo Fasulo <74818541+MatteoFasulo@users.noreply.github.com>
2021-12-22 12:41:13 +0100

    Add files via upload

--- Exam/main.R
+++ Exam/main.R
@@ -1,1 +1,25 @@
+load(url("https://github.com/smoxy/DMANA-proj/blob/main/Exam/DatiPresentazione2021.RData?raw=true"))
+source("https://raw.githubusercontent.com/MatteoFasulo/Rare-Earth/main/R/util/coreFunctions.R")
+loadPackages(c('ggplot2'))
 
+# Descrizione delle variabili
+# Pulizia del dataset (panel sbilanciato e NA) 
+# Utilizzando mice con imputazione multipla di tipo PMM per numeric e logreg per binarie
+# Misuriamo e bilanciamo i dati panel con PLM
+# GAMLSS
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
